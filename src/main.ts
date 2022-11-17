e6ae45c55754f86dc617c49e8efb8543926e57ee
Yue Dong <ydong011@fiu.edu>
2022-11-17 08:59:22 -0600

    update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
 
-  const port: number = 4231;
+  const port = 4231;
   await app.listen(port || 3000);
   logger.log(`Application listening on port ${port}`);
 }
